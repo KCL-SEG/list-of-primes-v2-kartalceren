f25243f85e9e7fcbb73781c0d6263037b2576ce6
Ceren Kartal (kartalceren) <ceren.s.kartal@kcl.ac.uk>
2022-12-09 13:43:28 +0000

    passed all prime tests

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    primes=[]
+    check = 2
+
+    if number_of_primes<=0:
+        raise ValueError
+
+
+    while len(primes) < number_of_primes:
+        primeTest = [check for i in primes if check%i == 0]
+        primes += [] if primeTest else [check]
+        check += 1
+    return primes
+
+
